b3b2dc95014e2e0b1a1943ffed1521c91792984b
Konrad Banachewicz <konrad@Konrads-MacBook-Pro.local>
2015-08-19 16:48:00 +0200

    seed fixing

--- xgboost_btb.R
+++ xgboost_btb.R
@@ -2,14 +2,42 @@
 library(readr)
 library(xgboost)
 
-set.seed(1)
+xseed <- 1
+set.seed(xseed)
 
 ## load and process data ####
-cat("reading the train and test data\n")
-xtrain <- read_csv(file = "./input/xtrain.csv")
+xtrain <- read_csv(file = "./input/train.csv")
 id_train <- xtrain$ID; xtrain$ID <- NULL
 y <- xtrain$target; xtrain$target <- NULL
+xtest <- read_csv(file = "./input/test.csv")
+id_test <- xtest$ID; xtest$ID <- NULL
 
+## preliminary preparation ####
+# drop constant columns
+is_missing <- colSums(is.na(xtrain))
+constant_columns <- which(is_missing == nrow(xtrain))
+xtrain <- xtrain[,-constant_columns]
+xtest <- xtest[,-constant_columns]
+rm(is_missing, constant_columns)
+
+# catch factor columns
+fact_cols <- which(lapply(xtrain, class) == "character")
+# map all categoricals into numeric => probably BS
+cat("assuming text variables are categorical & replacing them with numeric ids\n")
+for (f in colnames(xtrain)) {
+
+  if (class(xtrain[[f]])=="character") {
+    levels <- unique(c(xtrain[[f]], xtest[[f]]))
+    xtrain[[f]] <- as.integer(factor(xtrain[[f]], levels=levels))
+    xtest[[f]]  <- as.integer(factor(xtest[[f]],  levels=levels))
+  }
+}
+
+
+# handle NAs
+cat("replacing missing values with -1\n")
+xtrain[is.na(xtrain)] <- -1
+xtest[is.na(xtest)]   <- -1
 ## fit the model with early stopping ####
 set.seed(20150817)
 val_size <- 14000
@@ -23,13 +51,13 @@ watchlist <- list(val=xgval, train=xgtrain)
 # configure parameters
 param <- list(objective   = "binary:logistic",
               eval_metric = "auc",
-              "eta" = 0.05,
+              "eta" = 0.015,
               "min_child_weight" = 8,
-              "subsample" = .9, "colsample_bytree" = .7,
-              "max_depth" = 11,  "gamma" = 0.025, "silent" = 0)
+              "subsample" = .8, "colsample_bytree" = .7,
+              "max_depth" = 15,  "gamma" = 0.05, "silent" = 0)
 # fit the xgb
 clf <- xgb.train(params = param, data = xgtrain, 
-                 nround=350, print.every.n = 25, watchlist=watchlist, 
+                 nround=1200, print.every.n = 25, watchlist=watchlist, 
                  early.stop.round = 50, maximize = TRUE)
 
 
@@ -46,4 +74,5 @@ for (rows in split(1:nrow(xtest), ceiling((1:nrow(xtest))/10000))) {
 }
 
 cat("saving the submission file\n")
-write_csv(submission, "./submissions/xgboost_submission2_20150819.csv")
+fname <- paste("./submissions/xgboost_submission_seed",xseed,"_20150819.csv", sep = "")
+write_csv(submission, fname)

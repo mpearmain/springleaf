9134cb485f0d80476c552d957f9133bd76cde17f
Konrad Banachewicz <konrad@Konrads-MacBook-Pro.local>
2015-08-19 15:41:10 +0200

    run the xgboost on cleaned up dataset

--- xgboost_btb.R
+++ xgboost_btb.R
@@ -6,39 +6,13 @@ set.seed(1)
 
 ## load and process data ####
 cat("reading the train and test data\n")
-xtrain <- read_csv(file = "./input/train.csv")
+xtrain <- read_csv(file = "./input/xtrain.csv")
 id_train <- xtrain$ID; xtrain$ID <- NULL
 y <- xtrain$target; xtrain$target <- NULL
 
-xtest <- read_csv(file = "./input/test.csv")
-id_test <- xtest$ID; xtest$ID <- NULL
-
-## preliminary preparation ####
-# drop constant columns
-is_missing <- colSums(is.na(xtrain))
-constant_columns <- which(is_missing == nrow(xtrain))
-xtrain <- xtrain[,-constant_columns]
-xtest <- xtest[,-constant_columns]
-rm(is_missing, constant_columns)
-
-# map all categoricals into numeric => probably BS
-cat("assuming text variables are categorical & replacing them with numeric ids\n")
-for (f in colnames(xtrain)) {
-  if (class(xtrain[[f]])=="character") {
-    levels <- unique(c(xtrain[[f]], xtest[[f]]))
-    xtrain[[f]] <- as.integer(factor(xtrain[[f]], levels=levels))
-    xtest[[f]]  <- as.integer(factor(xtest[[f]],  levels=levels))
-  }
-}
-
-# handle NAs
-cat("replacing missing values with -1\n")
-xtrain[is.na(xtrain)] <- -1
-xtest[is.na(xtest)]   <- -1
-
 ## fit the model with early stopping ####
 set.seed(20150817)
-val_size <- 10000
+val_size <- 14000
 subrange <- sample(nrow(xtrain), size = val_size)
 
 # Set xgboost test and training and validation datasets
@@ -50,18 +24,20 @@ watchlist <- list(val=xgval, train=xgtrain)
 # configure parameters
 param <- list(objective   = "binary:logistic",
               eval_metric = "auc",
-              "eta" = 0.02,
+              "eta" = 0.05,
               "min_child_weight" = 8,
               "subsample" = .9, "colsample_bytree" = .7,
-              "max_depth" = 11,  "gamma" = 0.025, "silent" = 0)
+              "max_depth" = 9,  "gamma" = 0.025, "silent" = 0)
 # fit the xgb
 clf <- xgb.train(params = param, data = xgtrain, 
-                 nround=900, print.every.n = 25, watchlist=watchlist, 
+                 nround=350, print.every.n = 25, watchlist=watchlist, 
                  early.stop.round = 50, maximize = TRUE)
 
 
-
 ## generate submission on complete training set ####
+xtest <- read_csv(file = "./input/test.csv")
+id_test <- xtest$ID; xtest$ID <- NULL
+
 cat("making predictions in batches due to 8GB memory limitation\n")
 submission <- data.frame(ID=id_test)
 submission$target <- NA 

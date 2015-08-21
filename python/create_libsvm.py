319b040f7ecff7f7d19d8d183995d89fe1567a64
Mike Pearmain <michael.pearmain@gmail.com>
2015-08-21 15:53:33 +0100

    Adding wormhole conf folder for async ftrl /SGD/L-BGFS and FM.
    Adding libfm conversion script

---
+++ python/create_libsvm.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+__author__ = 'michaelpearmain'
+
+
+from csv import DictReader
+# target files
+
+# Data locations
+projPath = "/home/michaelpearmain/Kaggle/springleaf/"
+
+with open(projPath + 'input/xtrain.vw',"wb") as outfile:
+    for linenr, row in enumerate( DictReader(open(projPath + 'input/xtrain_v1.csv',"rb")) ):
+        n_d = ""
+        for kk in row.keys():
+            if kk == 'ID':
+                next
+            else:
+                if kk == 'target':
+                    label = 2 * int(row["target"]) - 1
+                else:
+                    # one-hot encode everything with hash trick
+                    n_d += " %s:1"%(abs(hash(kk + '_' + row[kk])))
+        outfile.write("%s %s \n"%(label, n_d))
+
+print("Finished Training data conversion")
+print("Starting Test data conversion")
+
+with open(projPath + 'input/xtest.vw',"wb") as outfile:
+    for linenr, row in enumerate( DictReader(open(projPath + 'input/xtest_v1.csv',"rb")) ):
+        n_d = ""
+        for kk in row.keys():
+            if kk == 'ID':
+                next
+            else:
+                # one-hot encode everything with hash trick
+                n_d += " %s:1"%(abs(hash(kk + '_' + row[kk])))
+                label = 0
+        outfile.write("%s %s \n"%(label, n_d))
--- python/sklearn-autotune.py
+++ python/sklearn-autotune.py
@@ -4,17 +4,17 @@ from __future__ import division
 __author__ = 'michael.pearmain'
 
 import autosklearn
-import numpy as np
 import pandas as pd
-from make_data import make_data
+from python.make_data import make_data
 
 if __name__ == "__main__":
     # Load data set and target values
-    train, train_labels, test, test_labels = make_data()
+    train, train_labels, test, test_labels = make_data(train_path = "../input/train.csv", test_path="../input/test.csv")
     automl = autosklearn.AutoSklearnClassifier()
     automl.fit(train, train_labels)
     print automl.score(test, test_labels)
 
-    submission = pd.DataFrame(automl.fit(train, train_labels).predict_proba(test.fillna(0))[:, 1], index=test.index, columns=['target'])
-    submission.index.name = 'ID'
-    submission.to_csv('../sklearn-autotune.csv')
+        print('Prediction Complete')
+    preds = automl.predict_proba(test)[:, 1]
+    submission = submission = pd.DataFrame(preds, index=test_labels, columns=['target'])
+    submission.to_csv('../xgb_autotune.csv')

f9e2dd1b0844c0e3186d05e83c7dbc4f97b101a3
Konrad Banachewicz <konrad@Konrads-MacBook-Pro.local>
2015-08-23 10:12:30 +0200

    further commenting out

--- R/features_selection.R
+++ R/features_selection.R
@@ -32,51 +32,51 @@ msg <- function(mmm,...)
 
 # iterate over features
 
-while (length(goodFeatures) < 35)
-{
-  relevMat <- rep(-10, ncol(xtrain))
-  for (jj in 1:ncol(xtrain))
-  {
-    if (!(jj %in% goodFeatures))
-    {
-      # surprisingly, overhead eats the whole advantage from parallelization
-      system.time(a <- foreach(ii = 1:length(idFix), .combine = c, .packages = c("Metrics", "glmnet")) %do%
-      {
-        idx <- idFix[[ii]]
-        y0 <- y[-idx]; y1 <- y[idx]
-        x0 <- xtrain[-idx, c(jj, goodFeatures)]
-        x1 <- xtrain[idx,c(jj, goodFeatures)]
-        mod0 <- glmnet(y = y0, x = as.matrix(x0), alpha = 0, family = "gaussian")
-        pred <- predict(mod0, as.matrix(x1))
-        Metrics::auc(y1,pred[,ncol(pred)])
-      }
-      
-	  )
-	  relevMat[jj] <- mean(a)
-      
-    }
-	if ((jj %% 300) == 0)
-	{
-		msg(jj)
-	}
-  }
-  goodFeatures <- c(goodFeatures, which.max(relevMat))
-  goodScore <- c(goodScore, max(relevMat))
-}
-
-rm(y0,y1,a,cl, col_types, fact_cols, idx, ii, is_missing,jj, mod0, pred)
-rm(xtrain_fc,x0,x1)
-
-xtrain <- xtrain[,goodFeatures]
-xtrain$ID <- id_train
-xtest$ID <- id_test
-xtrain$target <- y
-write_csv(xtrain, path = "./input/xtrain_r1.csv" )
-write_csv(xtest, path = "./input/xtest_r1.csv" )
-
-
-rm(id_test, id_train, idFix, msg, relevMat, xtest, xtrain,y)
-save.image("greedy_feature_selection.RData")
+# while (length(goodFeatures) < 35)
+# {
+#   relevMat <- rep(-10, ncol(xtrain))
+#   for (jj in 1:ncol(xtrain))
+#   {
+#     if (!(jj %in% goodFeatures))
+#     {
+#       # surprisingly, overhead eats the whole advantage from parallelization
+#       system.time(a <- foreach(ii = 1:length(idFix), .combine = c, .packages = c("Metrics", "glmnet")) %do%
+#       {
+#         idx <- idFix[[ii]]
+#         y0 <- y[-idx]; y1 <- y[idx]
+#         x0 <- xtrain[-idx, c(jj, goodFeatures)]
+#         x1 <- xtrain[idx,c(jj, goodFeatures)]
+#         mod0 <- glmnet(y = y0, x = as.matrix(x0), alpha = 0, family = "gaussian")
+#         pred <- predict(mod0, as.matrix(x1))
+#         Metrics::auc(y1,pred[,ncol(pred)])
+#       }
+#       
+# 	  )
+# 	  relevMat[jj] <- mean(a)
+#       
+#     }
+# 	if ((jj %% 300) == 0)
+# 	{
+# 		msg(jj)
+# 	}
+#   }
+#   goodFeatures <- c(goodFeatures, which.max(relevMat))
+#   goodScore <- c(goodScore, max(relevMat))
+# }
+# 
+# rm(y0,y1,a,cl, col_types, fact_cols, idx, ii, is_missing,jj, mod0, pred)
+# rm(xtrain_fc,x0,x1)
+# 
+# xtrain <- xtrain[,goodFeatures]
+# xtrain$ID <- id_train
+# xtest$ID <- id_test
+# xtrain$target <- y
+# write_csv(xtrain, path = "./input/xtrain_r1.csv" )
+# write_csv(xtest, path = "./input/xtest_r1.csv" )
+# 
+# 
+# rm(id_test, id_train, idFix, msg, relevMat, xtest, xtrain,y)
+# save.image("greedy_feature_selection.RData")
 
 ## feature selection - gbm-based ####
 # load the results from greedy feature selection

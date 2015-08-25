088966d3f77ca77c2f50bee15978e131d5787bee
Konrad Banachewicz <konrad@Konrads-MacBook-Pro.local>
2015-08-25 07:26:15 +0200

    work in progress

--- python/create_vw.py
+++ python/create_vw.py
@@ -31,11 +31,11 @@ time_columns = ["VAR0073","VAR0075","VAR0156","VAR0157","VAR0158","VAR0159",
                "VAR0166","VAR0167","VAR0168","VAR0169","VAR0176","VAR0177",
                "VAR0178","VAR0179","VAR0204","VAR0217"]
 
-with open(projPath + 'input/xvalid.vw',"wb") as outfile:
-    for linenr, row in enumerate( DictReader(open(projPath + 'input/xvalid_v4.csv',"rb")) ):
+with open(projPath + 'input/xtest.vw',"wb") as outfile:
+    for linenr, row in enumerate( DictReader(open(projPath + 'input/xtest_v4.csv',"rb")) ):
         # declare the contents of workspaces
         n_f = ""; n_a = ""; n_l = ""; n_j = ""; n_t = ""; n_r = ""
-
+        label = 1
         for kk in row.keys():
             if kk == 'ID':                
                 ID = row['ID']
@@ -58,3 +58,133 @@ with open(projPath + 'input/xvalid.vw',"wb") as outfile:
 
 
 # export PATH=/Users/konrad/Documents/vowpal_wabbit/vowpalwabbit:$PATH
+# vw -d ./input/xvalid.vw -i xmodel.vw -p predictions.txt
+
+#  -l [ --learning_rate ] arg            Set learning rate
+#  --power_t arg                         t power value
+#   --decay_learning_rate arg             Set Decay factor for learning_rate
+#                                        between passes
+#  -b [ --bit_precision ] arg            number of bits in the feature table
+#  --ngram arg                           Generate N grams. To generate N grams
+#                                        for a single namespace 'foo', arg
+#                                        should be fN.
+#  -q [ --quadratic ] arg                Create and use quadratic features
+#  --cubic arg                           Create and use cubic features
+
+#  -t [ --testonly ]                     Ignore label information and just test
+#  --holdout_off                         no holdout data in multiple passes
+#  --holdout_period arg                  holdout period for test only, default
+#                                        10
+#  --holdout_after arg                   holdout after n training examples,
+#                                        default off (disables holdout_period)
+#  --early_terminate arg                 Specify the number of passes tolerated
+#                                        when holdout loss doesn't decrease
+#                                        before early termination, default is 3
+#  --passes arg                          Number of Training Passes
+#  --l1 arg                              l_1 lambda
+#  --l2 arg                              l_2 lambda
+
+
+Output options:
+  -p [ --predictions ] arg              File to output predictions to
+  -r [ --raw_predictions ] arg          File to output unnormalized predictions
+                                        to
+
+Reduction options, use [option] --help for more info:
+
+  --bootstrap arg                       k-way bootstrap by online importance
+                                        resampling
+
+  --search arg                          Use learning to search,
+                                        argument=maximum action id or 0 for LDF
+
+  --cbify arg                           Convert multiclass on <k> classes into
+                                        a contextual bandit problem
+
+  --cb arg                              Use contextual bandit learning with <k>
+                                        costs
+
+  --csoaa_ldf arg                       Use one-against-all multiclass learning
+                                        with label dependent features.  Specify
+                                        singleline or multiline.
+
+  --wap_ldf arg                         Use weighted all-pairs multiclass
+                                        learning with label dependent features.
+                                          Specify singleline or multiline.
+
+  --csoaa arg                           One-against-all multiclass with <k>
+                                        costs
+
+  --multilabel_oaa arg                  One-against-all multilabel with <k>
+                                        labels
+
+  --log_multi arg                       Use online tree for multiclass
+
+  --ect arg                             Error correcting tournament with <k>
+                                        labels
+
+  --oaa arg                             One-against-all multiclass with <k>
+                                        labels
+
+  --top arg                             top k recommendation
+
+  --binary                              report loss as binary classification on
+                                        -1,1
+
+  --link arg (=identity)                Specify the link function: identity,
+                                        logistic or glf1
+
+  --stage_poly                          use stagewise polynomial feature
+                                        learning
+
+  --lrq arg                             use low rank quadratic features
+
+  --autolink arg                        create link function with polynomial d
+
+  --new_mf arg                          rank for reduction-based matrix
+                                        factorization
+
+  --nn arg                              Sigmoidal feedforward network with <k>
+                                        hidden units
+
+  --active                              enable active learning
+
+  --bfgs                                use bfgs optimization
+
+  --conjugate_gradient                  use conjugate gradient based
+                                        optimization
+
+  --lda arg                             Run lda with <int> topics
+
+  --noop                                do no learning
+
+  --print                               print examples
+
+  --rank arg                            rank for matrix factorization.
+
+  --sendto arg                          send examples to <host>
+
+  --svrg                                Streaming Stochastic Variance Reduced
+                                        Gradient
+
+  --ftrl                                FTRL: Follow the Proximal Regularized
+                                        Leader
+
+  --pistol                              FTRL: Parameter-free Stochastic
+                                        Learning
+
+  --ksvm                                kernel svm
+
+Gradient Descent options:
+  --sgd                                 use regular stochastic gradient descent
+                                        update.
+  --adaptive                            use adaptive, individual learning
+                                        rates.
+  --invariant                           use safe/importance aware updates.
+  --normalized                          use per feature normalized updates
+  --sparse_l2 arg (=0)                  use per feature normalized updates
+
+Input options:
+#  -c [ --cache ]                        Use a cache.  The default is                                        <data>.cache
+# --cache_file arg                      The location(s) of cache_file.#
+# -k [ --kill_cache ]                   do not reuse existing cache: create a

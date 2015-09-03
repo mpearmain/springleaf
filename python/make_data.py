61d32c2a9d9c42dc2d035839537d7f80d5281d72
Mike Pearmain <michael.pearmain@gmail.com>
2015-09-03 11:28:26 +0100

    Update to make_data() now runs for python and xgboost by LabelEncoder()

--- python/make_data.py
+++ python/make_data.py
@@ -11,36 +11,30 @@ def make_data(train_path, test_path):
     train = pd.read_csv(train_path).set_index("ID")
     test = pd.read_csv(test_path).set_index("ID")
 
-    x_train = train.drop('target',1).fillna(0)
+    x_train = train.drop('target',1)
     y_train = train.target
 
-    # Drop str cols.
-    # facVAR0008 facVAR0226 facVAR0230 facVAR0232 facVAR0236 facVAR0404 facVAR0493
+    # Use Label str cols.
 
-    feature_columns_to_use = ['Pclass','Sex','Age','Fare','Parch']
-    nonnumeric_columns = ['Sex']
+    names = list(x_train.columns.values)
+    nonnumeric_columns = filter(lambda x: 'fac' in x, names)
 
-    # Join the features from train and test together before imputing missing values,
-    # in case their distribution is slightly different
-    big_X = train_df[feature_columns_to_use].append(test_df[feature_columns_to_use])
+    # Join the features from train and test together for label encoding
+    big_X = x_train[names].append(test[names])
 
-    # XGBoost doesn't (yet) handle categorical features automatically, so we need to change
-    # them to columns of integer values.
-    # See http://scikit-learn.org/stable/modules/preprocessing.html#preprocessing for more
-    # details and options
+    # XGBoost doesn't (yet) handle categorical features automatically,
+    # so we need to change them to columns of integer values.
     le = LabelEncoder()
     for feature in nonnumeric_columns:
         big_X[feature] = le.fit_transform(big_X[feature])
 
-
     # Prepare the inputs for the model
-    train_X = big_X_imputed[0:train_df.shape[0]].as_matrix()
-    test_X = big_X_imputed[train_df.shape[0]::].as_matrix()
-    train_y = train_df['Survived']
+    x_train = big_X[0:x_train.shape[0]].as_matrix()
+    x_test = big_X[x_train.shape[0]::].as_matrix()
 
     print("Shape of x_train:", np.shape(x_train))
     print("Shape of y_train:", np.shape(y_train))
-    print("Shape of x_test:", np.shape(test.fillna(0)))
+    print("Shape of x_test:", np.shape(x_test))
     print("Shape of test_index:", np.shape(test.index))
 
-    return x_train, y_train, test.fillna(0), test.index
+    return x_train, y_train, x_test, test.index

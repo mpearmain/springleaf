68716ec29e186593d9d570f7b1f105d8468c8e14
Konrad Banachewicz <konrad@Konrads-MacBook-Pro.local>
2015-09-04 12:27:59 +0200

    cleanup

--- R/ensemble.R
+++ R/ensemble.R
@@ -4,8 +4,8 @@ library(data.table)
 
 # Best model so far.
 # Build a list of data sets.
-ensemble.data <- list(fread('./output/ftrl1sub.csv'),
-                      fread('./output/xgb_autotune.csv'))
+ensemble.data <- list(fread('~/Downloads/baggin5-ThreeBag-25082015.csv'),
+                      fread('~/Downloads/rf_minimix_20150824.csv'))
 # Rename the datasets to avoid name collisions in the merge.
 ensemble.data <- lapply(seq_along(ensemble.data), 
                         function(x) setnames(ensemble.data[[x]], c('ID', paste0('target', x))))
@@ -13,10 +13,9 @@ ensemble.data <- lapply(seq_along(ensemble.data),
 # Merge all the dataset together to ensemble.
 full.joins <- Reduce(function(x, y) merge(x, y, by='ID'), ensemble.data)
 
-
-alpha <- 0.1
+alpha <- 0.99
 full.joins[, target := (alpha * target1) + ((1 - alpha) * target2)]
 full.joins <- full.joins[, c('ID', 'target'), with = F]
-write.table(full.joins, file = "ensemble-19082015.csv", 
+write.table(full.joins, file = "xgboost3-5-bag-rf-minimix-082515.csv", 
             row.names = F, col.names = T, sep = ",", quote = F)
 

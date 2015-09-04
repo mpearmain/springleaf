4975cff9db0992f2ceacb09d7c64f6439b183d9f
Konrad Banachewicz <konrad@Konrads-MacBook-Pro.local>
2015-09-04 12:27:19 +0200

    btb - one more version

--- R/xgboost_btb.R
+++ R/xgboost_btb.R
@@ -1,71 +1,62 @@
 ## wd etc ####
 library(readr)
 library(xgboost)
+require(Matrix)
+require(caret)
 
 xseed <- 1
 set.seed(xseed)
 
 ## load and process data ####
-xtrain <- read_csv(file = "./input/train.csv")
+xtrain <- read_csv(file = "./input/xtrain_v5.csv")
 id_train <- xtrain$ID; xtrain$ID <- NULL
 y <- xtrain$target; xtrain$target <- NULL
-xtest <- read_csv(file = "./input/test.csv")
+xvalid <- read_csv(file = "./input/xvalid_v5.csv")
+id_valid <- xvalid$ID; xvalid$ID <- NULL
+y_valid <- xvalid$target; xvalid$target <- NULL
+xtest <- read_csv(file = "./input/xtest_v5.csv")
 id_test <- xtest$ID; xtest$ID <- NULL
 
-## preliminary preparation ####
-# drop constant columns
-is_missing <- colSums(is.na(xtrain))
-constant_columns <- which(is_missing == nrow(xtrain))
-xtrain <- xtrain[,-constant_columns]
-xtest <- xtest[,-constant_columns]
-rm(is_missing, constant_columns)
+## processing of factors
+# drop the job titles for now
+xtrain$facVAR0404 <- xtrain$facVAR0493 <- NULL
+xvalid$facVAR0404 <- xvalid$facVAR0493 <- NULL
+xtest$facVAR0404 <- xtest$facVAR0493 <- NULL
+# sparsify
+fact_cols <- grep("fac",colnames(xtrain))
+xtr <- xtrain[,fact_cols] ; xv <- xvalid[,fact_cols]; xte <- xtest[,fact_cols]
+xtrain <- xtrain[, -fact_cols]; xvalid <- xvalid[,-fact_cols]; xtest <- xtest[,-fact_cols]
+xd <- rbind(xtr, xv, xte)
+a <- sparse.model.matrix(~ . -1, data = xd); a <- as.matrix(a)
+flc <- findLinearCombos(a); a <- a[,-flc$remove]
+xtr_fc <- a[1:nrow(xtrain),]
+xv_fc <- a[(nrow(xtrain)+1):(nrow(xtrain) + nrow(xvalid)),]
+xte_fc <- a[(nrow(xtrain)+nrow(xvalid) + 1):nrow(a),]
+xtrain <- data.frame(xtrain, xtr_fc)
+xvalid <- data.frame(xvalid, xv_fc)
+xtest <- data.frame(xtest, xte_fc)
+rm(xte, xte_fc, xtr, xtr_fc, xv, xv_fc)
 
-# catch factor columns
-fact_cols <- which(lapply(xtrain, class) == "character")
-# map all categoricals into numeric => probably BS
-cat("assuming text variables are categorical & replacing them with numeric ids\n")
-for (f in colnames(xtrain)) {
-
-  if (class(xtrain[[f]])=="character") {
-    levels <- unique(c(xtrain[[f]], xtest[[f]]))
-    xtrain[[f]] <- as.integer(factor(xtrain[[f]], levels=levels))
-    xtest[[f]]  <- as.integer(factor(xtest[[f]],  levels=levels))
-  }
-}
-
-
-# handle NAs
-cat("replacing missing values with -1\n")
-xtrain[is.na(xtrain)] <- -1
-xtest[is.na(xtest)]   <- -1
-## fit the model with early stopping ####
-set.seed(20150817)
-val_size <- 14000
-subrange <- sample(nrow(xtrain), size = val_size)
-
-# Set xgboost test and training and validation datasets
-xgtrain <- xgb.DMatrix(data = as.matrix(xtrain)[-subrange,], label= y[-subrange])
-xgval <-  xgb.DMatrix(data = as.matrix(xtrain)[subrange,], label= y[subrange])
-watchlist <- list(val=xgval, train=xgtrain)
+## xgboost  ####
+#Set xgboost test and training and validation datasets
+xgtrain <- xgb.DMatrix(data = as.matrix(xtrain), label= y)
+xgval <-  xgb.DMatrix(data = as.matrix(xvalid), label= y_valid)
+watchlist <- list(val=xgval)
+rm(xtrain, xvalid)
 
 # configure parameters
 param <- list(objective   = "binary:logistic",
               eval_metric = "auc",
-              "eta" = 0.01,
+              "eta" = 0.004,
               "min_child_weight" = 8,
-              "subsample" = .8, "colsample_bytree" = .6,
-              "max_depth" = 15,  "gamma" = 0.05, "silent" = 0)
+              "subsample" = .7, "colsample_bytree" = .7,
+              "max_depth" = 20,  "gamma" = 0.1, "silent" = 0)
 # fit the xgb
 clf <- xgb.train(params = param, data = xgtrain, 
-                 nround=1800, print.every.n = 25, watchlist=watchlist, 
+                 nround=5000, print.every.n = 25, watchlist=watchlist, 
                  early.stop.round = 50, maximize = TRUE)
 
-
-## generate submission on complete training set ####
-xtest <- read_csv(file = "./input/test.csv")
-id_test <- xtest$ID; xtest$ID <- NULL
-xgtest <- xgb.DMatrix(data = as.matrix(xtest))
-
+## generate submission on complete test set ####
 cat("making predictions in batches due to 8GB memory limitation\n")
 submission <- data.frame(ID=id_test)
 submission$target <- NA 
@@ -74,5 +65,5 @@ for (rows in split(1:nrow(xtest), ceiling((1:nrow(xtest))/10000))) {
 }
 
 cat("saving the submission file\n")
-fname <- paste("./submissions/xgboost_submission_seed",xseed,"_20150820.csv", sep = "")
+fname <- paste("./submissions/xgboost_dataV5_20150901.csv", sep = "")
 write_csv(submission, fname)

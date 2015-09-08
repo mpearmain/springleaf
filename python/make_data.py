51926f1a4f34429e0b40e18f6e58c966a4674ce8
mpearmain <michael.pearmain@gmail.com>
2015-09-08 14:30:00 +0100

    changes to make_data() to support str cols from data_preperation.R New one transformations and one hot encoding for Keras Test

--- python/make_data.py
+++ python/make_data.py
@@ -5,7 +5,7 @@ __author__ = 'michael.pearmain'
 
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.feature_extraction import DictVectorizer
 
 def one_hot_dataframe(data, cols, replace=False):
@@ -72,14 +72,15 @@ def make_data_scaled_one_hot(train_path, test_path):
     # Join the features from train and test together.
     big_X = x_train[names].append(test[names])
     # Drop all but dmp cols
-    dmp_cols = filter(lambda x: 'dmp' not in x, fac_columns)
+    dmp_cols = filter(lambda x: 'dmp' in x, fac_columns)
     for feature in list(set(names) - set(dmp_cols)):
-        big_X = big_X.drop(feature,1)
+        big_X = big_X.drop(feature, 1)
 
+    print("Shape of Data after dropping cols:", np.shape(big_X))
     # Rescale all numeric cols to be between 0 and 1.
-    min_max_scaler = MinMaxScaler()
+    scaler = StandardScaler()
     for feature in dmp_cols:
-        big_X[feature] = min_max_scaler.fit_transform(big_X[feature])
+        big_X[feature] = scaler.fit_transform(big_X[feature])
 
     # One hot encode the categorical vars in place.
     #big_X, _ = one_hot_dataframe(big_X, cols=nonnumeric_columns,replace=True)

9be72e46d6fed942f1fb9e5ee0b8f37b5601c3dc
Mike Pearmain <michael.pearmain@gmail.com>
2015-09-10 09:42:57 +0100

    Subtle tify up changes to make daa and kerasNN for easy debug and explaination

--- python/kerasNN.py
+++ python/kerasNN.py
@@ -4,7 +4,6 @@ from __future__ import division
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
-from sklearn.preprocessing import StandardScaler
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation
 from keras.layers.normalization import BatchNormalization
@@ -13,7 +12,7 @@ from keras.utils import np_utils
 from sklearn import metrics
 from sklearn.cross_validation import KFold
 
-from make_data import make_data_scaled_one_hot
+from make_data import make_data_keras
 
 
 '''
@@ -30,50 +29,37 @@ def float32(k):
 
 def build_model(input_dim, output_dim):
     model = Sequential()
-    model.add(Dropout(0.1))
+    model.add(Dropout(0.2))
     model.add(Dense(input_dim, 1999, init='glorot_uniform'))
     model.add(PReLU((1999,)))
-    #model.add(BatchNormalization((1999,)))
     model.add(Dropout(0.3))
 
     model.add(Dense(1999, 1500, init='glorot_uniform'))
     model.add(PReLU((1500,)))
-    #model.add(BatchNormalization((1500,)))
     model.add(Dropout(0.3))
 
     model.add(Dense(1500, 500, init='glorot_uniform'))
     model.add(PReLU((500,)))
-    #model.add(BatchNormalization((1500,)))
-    model.add(Dropout(0.5))
+    model.add(Dropout(0.3))
 
     model.add(Dense(500, output_dim, init='glorot_uniform'))
     model.add(Activation('sigmoid'))
 
-    model.compile(loss='binary_crossentropy', optimizer="adadelta")
+    model.compile(loss='binary_crossentropy', optimizer="adagrad")
     return model
 
 
 if __name__ == "__main__":
     # Load data set and target values
     x_train, Y, X_test, ids = \
-        make_data_scaled_one_hot(train_path = "../input/xtrain_v6.csv",
+        make_data_keras(train_path = "../input/xtrain_v6.csv",
                   test_path="../input/xtest_v6.csv")
 
-    print(x_train.shape)
-    #np.random.shuffle(x_train)
-
-    x_train = x_train.astype(np.float32)
     encoder = LabelEncoder()
     y = encoder.fit_transform(Y).astype(np.int32)
-    scaler = StandardScaler()
-    x_train = scaler.fit_transform(x_train)
-
-
-    X_test, = X_test.astype(np.float32),
-    X_test = scaler.transform(X_test)
 
     '''Convert class vector to binary class matrix,
-    for use with categorical_crossentropy'''
+    for use with binary_crossentropy'''
     Y = np_utils.to_categorical(y)
 
     print('Number of classes:', len(encoder.classes_))
@@ -104,7 +90,7 @@ if __name__ == "__main__":
 
         print("Training model...")
 
-        model.fit(X_train, Y_train, nb_epoch=8, batch_size=24,
+        model.fit(X_train, Y_train, nb_epoch=10, batch_size=132,
                   validation_data=(X_valid, Y_valid), verbose=1)
         valid_preds = model.predict_proba(X_valid, verbose=0)
         valid_preds = valid_preds[:, 1]
@@ -117,8 +103,8 @@ if __name__ == "__main__":
     print("Generating submission...")
 
     model = build_model(input_dim, output_dim)
-    model.fit(x_train, Y, nb_epoch=8, batch_size=24)
+    model.fit(x_train, Y, nb_epoch=10, batch_size=132)
 
     preds = model.predict_proba(X_test, verbose=0)[:, 1]
     submission = pd.DataFrame(preds, index=ids, columns=['target'])
-    submission.to_csv('Keras_BTB.csv')    
+    submission.to_csv('Keras_BTB.csv')
--- python/make_data.py
+++ python/make_data.py
@@ -5,14 +5,17 @@ __author__ = 'michael.pearmain'
 
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelEncoder, MinMaxScaler
+from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn.feature_extraction import DictVectorizer
+
+''' Below are two functions for creating data,
+ make_data() converts data for using with xgboost and other sklearn functions
+ make_data_keras() is specific to keras NN
+'''
 
 def one_hot_dataframe(data, cols, replace=False):
     """ Takes a dataframe and a list of columns that need to be encoded.
-    Returns a 3-tuple comprising the data, the vectorized data,
-    and the fitted vectorizor.
-    Modified from https://gist.github.com/kljensen/5452382
+    Returns a 2-tuple comprising of the data, the vectorized data.
     """
     vec = DictVectorizer()
     vecData = pd.DataFrame(vec.fit_transform(data[cols].to_dict(orient='records')).toarray())
@@ -56,47 +59,34 @@ def make_data(train_path, test_path):
 
     return x_train, y_train, x_test, test.index
 
-def make_data_scaled_one_hot(train_path, test_path):
+def make_data_keras(train_path, test_path):
+    # Load the data and set the index for the pandas data
     train = pd.read_csv(train_path).set_index("ID")
     test = pd.read_csv(test_path).set_index("ID")
+    # Drop the target var
     x_train = train.drop('target',1)
     y_train = train.target
+    # Clean up train
+    del train
 
-    # Use Label str cols.
-    names = list(x_train.columns.values)
-    fac_columns = filter(lambda x: 'dmp' in x, names)
-    diff_columns = filter(lambda x: 'diff' in x, names)
-    sum_columns = filter(lambda x: 'sum' in x, names)
-
-    proc = set()
-    proc.update(fac_columns)
-    proc.update(diff_columns)
-    proc.update(sum_columns)
-
-    # Remove facVAR cols from list
-    nonnumeric_columns = filter(lambda x: 'fac' in x, names)
-    nonnumeric_columns = filter(lambda x: 'dmp' not in x, nonnumeric_columns)
+    names = x_train.columns.values
     # Join the features from train and test together.
     big_X = x_train[names].append(test[names])
     print("Shape of Data bfore dropping cols:", np.shape(big_X))
+
+    # Remove facVAR cols from data but keep the facVARdmp vars to model them.
+    # One-hot encoding the categorical data produces huge data bloat
+    # We have continuous vars to use instead.
+    nonnumeric_columns = filter(lambda x: 'fac' in x, names)
+    nonnumeric_columns = filter(lambda x: 'dmp' not in x, nonnumeric_columns)
+
     big_X = big_X.drop(nonnumeric_columns, 1)
-    #big_X = big_X.drop(list(set(list(big_X.columns.values)) - set(proc)), 1)
-
     print("Shape of Data after dropping cols:", np.shape(big_X))
-    # Rescale all numeric cols to be between 0 and 1.
-    names = list(big_X.columns.values)
-    # scaler = MinMaxScaler()
-    # for feature in names:
-    #     big_X[feature] = scaler.fit_transform(big_X[feature])
 
-    # One hot encode the categorical vars in place.
-    #big_X, _ = one_hot_dataframe(big_X, cols=nonnumeric_columns,replace=True)
-
-    # Scale all
-    # le = LabelEncoder()
-    # for feature in nonnumeric_columns:
-    #     big_X[feature] = le.fit_transform(big_X[feature])
-
+    # Rescale all cols.
+    big_X = big_X.astype(np.float32)
+    scaler = StandardScaler()
+    big_X = scaler.fit_transform(big_X)
 
     # Prepare the inputs for the model
     x_train = big_X[0:x_train.shape[0]]

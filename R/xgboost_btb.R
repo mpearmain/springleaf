e7d879e91448a64c89fb7b503397212bd6ea1f9f
tng-konrad <konrad@Konrads-MacBook-Pro.local>
2015-10-05 20:57:42 +0200

    modified to generate valid / test preds, parametrized by date, seed and datasetversion

--- R/xgboost_btb.R
+++ R/xgboost_btb.R
@@ -4,21 +4,23 @@ library(xgboost)
 require(Matrix)
 require(caret)
 
-xseed <- 19
+xseed <- 123
+vname <- "v9_r7"
+
 set.seed(xseed)
 
 ## load and process data ####
-xtrain <- read_csv(file = "./input/xtrain_v9_r1.csv")
+xtrain <- read_csv(file = paste("./input/xtrain_",vname,".csv", sep = ""))
 id_train <- xtrain$ID; xtrain$ID <- NULL
 y <- xtrain$target; xtrain$target <- NULL
-xtest <- read_csv(file = "./input/xtest_v9_r1.csv")
+xtest <- read_csv(file = paste("./input/xtest_",vname,".csv", sep = ""))
 id_test <- xtest$ID; xtest$ID <- NULL
 
 ## xgboost  ####
 #Set xgboost test and training and validation datasets
 xfold <- read_csv(file = "./input/xfolds.csv")
 subrange <- which(xfold$valid == 1)
-xvalid <- xtrain[subrange,]; y_valid <- y[subrange]
+xvalid <- xtrain[subrange,]; y_valid <- y[subrange]; id_valid <- id_train[subrange]
 xtrain <- xtrain[-subrange,]; y <- y[-subrange]
 xgtrain <- xgb.DMatrix(data = as.matrix(xtrain), label= y)
 xgval <-  xgb.DMatrix(data = as.matrix(xvalid), label= y_valid)
@@ -37,14 +39,20 @@ clf <- xgb.train(params = param, data = xgtrain,
                  nround=5000, print.every.n = 25, watchlist=watchlist, 
                  early.stop.round = 50, maximize = TRUE)
 
-## generate submission on complete test set ####
+## generate submission on validation and test sets ####
+# prediction on test set
 cat("making predictions in batches due to 8GB memory limitation\n")
-submission <- data.frame(ID=id_test)
-submission$target <- NA 
+submission <- data.frame(ID=id_test); submission$target <- NA 
 for (rows in split(1:nrow(xtest), ceiling((1:nrow(xtest))/10000))) {
   submission[rows, "target"] <- predict(clf, data.matrix(xtest[rows,]))
 }
-
 cat("saving the submission file\n")
-fname <- paste("./submissions/xgboost_dataV9_20151002.csv", sep = "")
+fname <- paste("./submissions/predFull_data",stringr::str_replace(vname, "_",""),"_seed",xseed,"_20151004.csv", sep = "")
+write_csv(submission, fname)
+
+# prediction on validation set
+submission <- data.frame(ID=id_valid); submission$target <- NA 
+pred <- predict(clf, xgval)
+submission$target <- pred
+fname <- paste("./submissions/predValid_data",stringr::str_replace(vname, "_",""),"_seed",xseed,"_20151004.csv", sep = "")
 write_csv(submission, fname)
